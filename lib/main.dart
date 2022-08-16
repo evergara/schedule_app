4b0009bb7578e3a3ad1d2a52caad3b92ab6f64f6
Emerson Vergara <emerson.vergara@ceiba.com.co>
2022-08-16 15:34:35 -0500

    Configuracion de dependencias firebase y egregar maneno de multiDexEnabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:schedule_app/di/di_container.dart';
 import 'package:schedule_app/schedule_app.dart';
@@ -5,6 +6,8 @@ import 'package:schedule_app/schedule_app.dart';
 import 'data/repositories/task_repository_memory.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   initDIContainer();
   runApp(const ScheduleApp());
   /*
